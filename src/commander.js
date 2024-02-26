5d0ad60f07dfa8eaee651e3135b0fd5cea8d02e6
Андрей Серебренников <a.serebrennikov1986@gmail.com>
2024-02-26 11:30:16 +0500

    added argument file commander.js

--- src/commander.js
+++ src/commander.js
@@ -2,8 +2,11 @@ import { Option, program } from 'commander';
 
 program
     .name('gendiff')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1')
+    .option('-f, --format [type]', 'output format')
     .addHelpOption(new Option('-h, --help', 'output usage information'))
 
 const getHelp = program.parse();  
