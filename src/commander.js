6c7df80ac78583c8fa23b868b3f22f60310a913b
Андрей Серебренников <a.serebrennikov1986@gmail.com>
2024-10-09 13:31:31 +0500

    added comander.js

---
+++ src/commander.js
@@ -0,0 +1,12 @@
+import { program } from 'commander';
+
+const getCertificate = () => {
+  program
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-V, --version', 'output the version number')
+    .helpOption('-h, --help', 'output usage information');
+  program.parse();
+};
+
+export default getCertificate;
