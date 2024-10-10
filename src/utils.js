991a64a4e3593f9d6f2af988c301e1df8c425a5f
Андрей Серебренников <a.serebrennikov1986@gmail.com>
2024-10-10 18:58:55 +0500

    update utils.js

--- src/utils.js
+++ src/utils.js
@@ -3,18 +3,18 @@
 import * as path from 'path';
 import { readFileSync } from 'node:fs';
 import { cwd } from 'node:process';
+import parse from './parser.js';
 
 const getData = (filepath) => {
+  const format = path.extname(filepath);
   const absolutePath = path.resolve(cwd(), filepath);
-  return readFileSync(absolutePath, 'utf-8', (err, data) => {
-    if (err) throw err;
-    console.log(JSON.parse(data));
-  });
+  const content = readFileSync(absolutePath, 'utf-8');
+  return parse(content, format);
 };
 
 const command = (path1, path2) => {
-  const dataFile1 = JSON.parse(getData(path1));
-  const dataFile2 = JSON.parse(getData(path2));
+  const dataFile1 = getData(path1);
+  const dataFile2 = getData(path2);
   const keys = [...new Set(Object.keys(dataFile1).concat(Object.keys(dataFile2)))].sort();
   const result = keys.reduce((acc, key) => {
     if (Object.hasOwn(dataFile2, key) && dataFile1[key] === dataFile2[key]) {
@@ -29,5 +29,6 @@ const command = (path1, path2) => {
     return acc;
   }, '');
   console.log(`{\n${result}}`);
+  return `{\n${result}}`;
 };
 export default command;
