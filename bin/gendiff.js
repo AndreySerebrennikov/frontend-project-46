1fc1e43fa513c7f8256dfec7c6338e30ef913e2a
Андрей Серебренников <a.serebrennikov1986@gmail.com>
2024-10-10 19:00:11 +0500

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
-import getCertificate from '../src/commander.js';
+import { program } from 'commander';
+import command from '../src/utils.js';
 
-getCertificate();
+program
+  .name('gendiff')
+  .argument('filepath1')
+  .argument('filepath2')
+  .action(command)
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .helpOption('-h, --help', 'output usage information');
+program.parse();
