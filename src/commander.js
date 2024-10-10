0c9de7cc6ff1d51ed5275ddb9207014b8bc3f6c1
Андрей Серебренников <a.serebrennikov1986@gmail.com>
2024-10-10 10:03:33 +0500

    update commander.js

--- src/commander.js
+++ src/commander.js
@@ -1,10 +1,12 @@
 import { program } from 'commander';
+import command from './command.js';
 
 const getCertificate = () => {
   program
     .name('gendiff')
-    .arguments('filepath1')
-    .arguments('filepath2')
+    .argument('filepath1')
+    .argument('filepath2')
+    .action(command)
     .description('Compares two configuration files and shows a difference.')
     .option('-V, --version', 'output the version number')
     .option('-f, --format [type]', 'output format')
