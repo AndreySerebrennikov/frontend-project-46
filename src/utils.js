052eab897720461c6ff871e20f68eebedb4257e4
Андрей Серебренников <a.serebrennikov1986@gmail.com>
2024-10-16 18:09:52 +0500

    update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,34 +1,37 @@
-/* eslint-disable no-param-reassign */
-
 import * as path from 'path';
 import { readFileSync } from 'node:fs';
 import { cwd } from 'node:process';
 import parse from './parser.js';
+import stilysh from '../format/stilysh.js';
 
-const getData = (filepath) => {
+export const getData = (filepath) => {
   const format = path.extname(filepath);
   const absolutePath = path.resolve(cwd(), filepath);
   const content = readFileSync(absolutePath, 'utf-8');
   return parse(content, format);
 };
 
-const command = (path1, path2) => {
-  const dataFile1 = getData(path1);
-  const dataFile2 = getData(path2);
-  const keys = [...new Set(Object.keys(dataFile1).concat(Object.keys(dataFile2)))].sort();
-  const result = keys.reduce((acc, key) => {
-    if (Object.hasOwn(dataFile2, key) && dataFile1[key] === dataFile2[key]) {
-      acc += `    ${key}: ${dataFile1[key]}\n`;
+const getGenDiff = (file1, file2) => {
+  const keys = [...new Set(Object.keys(file1).concat(Object.keys(file2)))].sort();
+  return keys.map((key) => {
+    if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
+      if (typeof file1[key] === 'object' && typeof file2[key] === 'object') {
+        return { key: `${key}`, type: 'nested', children: getGenDiff(file1[key], file2[key]) };
+      }
     }
-    if (Object.hasOwn(dataFile1, key) && dataFile1[key] !== dataFile2[key]) {
-      acc += `  - ${key}: ${dataFile1[key]}\n`;
+    if (!Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
+      return { key: `${key}`, type: 'added', value: file2[key] };
     }
-    if (Object.hasOwn(dataFile2, key) && dataFile1[key] !== dataFile2[key]) {
-      acc += `  + ${key}: ${dataFile2[key]}\n`;
+    if (Object.hasOwn(file1, key) && (!Object.hasOwn(file2, key))) {
+      return { key: `${key}`, type: 'deleted', value: file1[key] };
     }
-    return acc;
-  }, '');
-  console.log(`{\n${result}}`);
-  return `{\n${result}}`;
+    if ((Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) && (file1[key] !== file2[key])) {
+      return {
+        key: `${key}`, type: 'changed', value1: file1[key], value2: file2[key],
+      };
+    }
+    return { key: `${key}`, type: 'unchanged', value: file1[key] };
+  });
 };
+const command = (path1, path2) => stilysh(getGenDiff(getData(path1), getData(path2)));
 export default command;
