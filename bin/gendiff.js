d3f23ab7fed1f03a8ab469659067dcfa97048aec
Андрей Серебренников <a.serebrennikov1986@gmail.com>
2024-10-28 12:55:36 +0500

    updated gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,12 +6,11 @@ program
   .name('gendiff')
   .argument('filepath1')
   .argument('filepath2')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number')
-  .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'output usage information')
-  .action((filepath1, filepath2) => {
-    const result = command(filepath1, filepath2);
+  .action((filepath1, filepath2, options) => {
+    const result = command(filepath1, filepath2, options.format);
     console.log(result);
   });
 program.parse();
