c1a04d718b9f96094e71da0e68be7c11608179aa
Андрей Серебренников <a.serebrennikov1986@gmail.com>
2024-10-28 13:22:36 +0500

    updated index.js

--- formatters/index.js
+++ formatters/index.js
@@ -1,9 +1,13 @@
 import stilysh from './stilysh.js';
 import plain from './plain.js';
+import json from './json.js';
 
 const getFormatter = (data, formatName) => {
   if (formatName === 'plain') {
     return plain(data);
+  }
+  if (formatName === 'json') {
+    return json(data);
   }
   return stilysh(data);
 };
